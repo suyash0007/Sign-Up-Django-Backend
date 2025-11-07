81530f3b3d0c14b5a79ab47313f6abce6a3a05d2
Suyash <tiwarisuyash40@gmail.com>
2025-11-07 13:34:55 +0530

    fix3

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import a_module_that_does_not_exist
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
